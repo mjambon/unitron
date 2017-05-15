b5b3ea1ef390833c699ccd070fe68fbec410de52
Martin Jambon <martin@mjambon.com>
2017-05-14 17:09:32 -0700

    Comment on what to implement next

--- u_system.ml
+++ u_system.ml
@@ -17,6 +17,13 @@ type t = {
 
 (*
    Run one cycle at time t.
+
+   1. Activate controls (get their list).
+   2. Record active controls (acts) and keep them until they're older
+      than some max.
+   3. Perform the actions triggered by the controls.
+   4. Collect feedback from the goal function.
+   5. Decompose feedback as a sum of contributions from all recent acts.
 *)
-let tick x t =
+let step x t =
   ()
