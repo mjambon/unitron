bed7610fe04f90f4350ffeca0cb5b2b7ea403548
Martin Jambon <martin@mjambon.com>
2017-05-27 17:03:25 -0700

    Reinforcement (in progress)

--- u_learn.ml
+++ u_learn.ml
@@ -2,5 +2,16 @@
    Reinforcement step.
 *)
 
+open U_system
+
 let learn (x : U_system.t) (feedback : float) =
+  let contributions =
+    U_recent_acts.fold x.recent_acts [] (fun age controlid acc ->
+      let control = x.get_control controlid in
+      let contribution = U_control.(control.contributions).(age) in
+      contribution :: acc
+    )
+  in
+  (* TODO: adjust each contribution based on feedback *)
   ()
+
